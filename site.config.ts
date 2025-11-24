6b6b7fd0dbe99f8f6a6de73c6ba6780340597082
Peng Lu <150557980+PNGL9527@users.noreply.github.com>
2025-11-24 16:14:34 +0100

    Update site.config.ts

--- site.config.ts
+++ site.config.ts
@@ -50,16 +50,12 @@ export default siteConfig({
 
   // whether to use the default notion navigation style or a custom one with links to
   // important pages. To use `navigationLinks`, set `navigationStyle` to `custom`.
-  navigationStyle: 'default'
-  // navigationStyle: 'custom',
-  // navigationLinks: [
-  //   {
-  //     title: 'About',
-  //     pageId: 'f1199d37579b41cbabfc0b5174f4256a'
-  //   },
-  //   {
-  //     title: 'Contact',
-  //     pageId: '6a29ebcb935a4f0689fe661ab5f3b8d1'
-  //   }
-  // ]
+  // navigationStyle: 'default'
+  navigationStyle: 'custom',
+  navigationLinks: [
+  {
+    title: 'My CV',
+    url: 'https://github.com/PNGL9527/notion/blob/main/public/Peng%20Lu_CV_EN%26IT.pdf' // PDF 放在 public 文件夹
+  }
+  ]
 })
