9a219b58cf39d8b15a45f310ca2bf1e011fcb0a1
Peng Lu <150557980+PNGL9527@users.noreply.github.com>
2025-11-24 16:51:18 +0100

    Update PageAside.tsx

--- components/PageAside.tsx
+++ components/PageAside.tsx
@@ -1,7 +1,6 @@
 import { type Block, type ExtendedRecordMap } from 'notion-types'
 
 import { getPageTweet } from '@/lib/get-page-tweet'
-
 import { PageActions } from './PageActions'
 import { PageSocial } from './PageSocial'
 
@@ -18,9 +17,9 @@ export function PageAside({
     return null
   }
 
-  // only display comments and page actions on blog post pages
   if (isBlogPost) {
     const tweet = getPageTweet(block, recordMap)
+
     if (!tweet) {
       return null
     }
